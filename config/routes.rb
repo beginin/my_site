bf1376933368788ae7a8adb488e67bf000442638
root <root@development.(none)>
2012-12-05 17:01:56 +0800

    Add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MySite::Application.routes.draw do
+  get "welcome/index"
+
+  get "welcome/help"
+
+  get "welcome/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
