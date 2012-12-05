536f9564fa9c8828fbd5b2b00b56aaf24b8288b5
root <root@development.(none)>
2012-12-05 10:58:23 +0800

    Commmit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MySite::Application.config.secret_token = '8249de0f6221ca84067cb9c581cfaaf23e738794a6bb4fc70ffdf0fb9caee823f2e5e6044211b6c59d6846bfd7fda386b23d0bf943ea4613eb89f1e4965b05d4'
