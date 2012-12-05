50afc6014f6c58a795cc60482c8bdf7fa3c81f86
root <root@development.(none)>
2012-12-05 12:14:48 +0800

    Change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,13 +22,13 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
+    run "cd #{current_path}/ && rake assets:precompile"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    run "cd #{release_path}/ && rake assets:precompile"  
   end
   after "deploy:finalize_update", "deploy:symlink_config"
   desc "Make sure local git is in sync with remote."
