9712709fc3c6a71ee770b44aaa15fdf0a1a0ffb2
root <root@development.(none)>
2012-12-05 12:04:04 +0800

    Ce

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ namespace :deploy do
   task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/master`
       puts "WARNING: HEAD is not the same as origin/master"
-      puts "Run `git push` to sync changes."
+      puts "Run `git  push` to sync changes."
       exit
     end
   end
