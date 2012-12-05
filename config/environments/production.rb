c80994395a72e55be4b6ce1b354ff9a7beca2ee8
root <root@development.(none)>
2012-12-05 16:20:23 +0800

    Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,8 @@ MySite::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  #config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true
@@ -61,10 +62,6 @@ MySite::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 	
-config.assets.compile = true
-config.consider_all_requests_local = false
-config.action_controller.perform_caching = true
-config.serve_static_assets = true
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
